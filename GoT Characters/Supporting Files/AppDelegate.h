f0fbf3c579a5831c231f5811a8966402ca2b10be
Mateusz Karwat <mateuszkarwat@gmail.com>
2016-11-20 12:40:18 +0100

    Create initial project

---
+++ GoT Characters/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoT Characters
+//
+//  Created by Mateusz Karwat on 20/11/2016.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoT Characters/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoT Characters
+//
+//  Created by Mateusz Karwat on 20/11/2016.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
